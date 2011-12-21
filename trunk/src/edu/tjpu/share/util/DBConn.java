4a38d9376d3155a8ad8ae1f06f89dec7e1a17f76
tonyduandi@gmail.com <tonyduandi@gmail.com@8dba909f-7c90-063c-3fb1-551c1a91ad12>
2011-12-21 02:55:13 +0000

    增加数据库连接池优化处理速度

--- trunk/src/edu/tjpu/share/util/DBConn.java
+++ trunk/src/edu/tjpu/share/util/DBConn.java
@@ -1,51 +1,73 @@
 package edu.tjpu.share.util;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class DBConn {
-	
+
 	private Connection conn = null;
 	private PreparedStatement pstmt = null;
 	private ResultSet rs = null;
 
 	public DBConn() {
+		// try {
+		// Class.forName(Config.CLASS_NAME);
+		//
+		// String url =
+		// Config.DATABASE_URL+"://"+Config.SERVER_IP+":"+Config.SERVER_PORT+"/"+Config.DATABASE_SID;
+		// conn =
+		// DriverManager.getConnection(url,Config.USERNAME,Config.PASSWORD);
+		// } catch (ClassNotFoundException e) {
+		// e.printStackTrace();
+		// } catch (SQLException e) {
+		// e.printStackTrace();
+		// }
+		
+		String jndi = "jdbc/test"; 
 		try {
-			Class.forName(Config.CLASS_NAME);
-			
-			String url = Config.DATABASE_URL+"://"+Config.SERVER_IP+":"+Config.SERVER_PORT+"/"+Config.DATABASE_SID;
-			conn = DriverManager.getConnection(url,Config.USERNAME,Config.PASSWORD);
-		} catch (ClassNotFoundException e) {
+			Context initContext = new InitialContext();
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");//固定，不需要修改
+			DataSource ds = (DataSource)envContext.lookup(jndi);
+			if(ds != null){
+			conn = ds.getConnection();
+			}
+		}catch (SQLException e) {
 			e.printStackTrace();
-		} catch (SQLException e) {
+		} catch (NamingException e) {
 			e.printStackTrace();
-		}		
+		}
 	}
 
 	public Connection getConn() {
+		
+		  
 		return conn;
 	}
-	
-	public void closeConn(){
-		if(rs!=null){
+
+	public void closeConn() {
+		if (rs != null) {
 			try {
 				rs.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
-		if(pstmt!=null){
+		if (pstmt != null) {
 			try {
 				pstmt.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
-		if(conn!=null){
+		if (conn != null) {
 			try {
 				conn.close();
 			} catch (SQLException e) {
@@ -54,19 +76,15 @@ public class DBConn {
 		}
 	}
 
-    
-	
-	public int execOther(final String strSQL,final Object[] params ){
+	public int execOther(final String strSQL, final Object[] params) {
 
-	
-
-		System.out.println("SQL:> "+strSQL);		
+		System.out.println("SQL:> " + strSQL);
 		try {
 
 			pstmt = conn.prepareStatement(strSQL);
-			//4����̬Ϊpstmt���ֵ
-			for(int i=0; i< params.length ;i++){
-				pstmt.setObject(i+1, params[i]);
+			// 4����̬Ϊpstmt���ֵ
+			for (int i = 0; i < params.length; i++) {
+				pstmt.setObject(i + 1, params[i]);
 			}
 
 			int affectedRows = pstmt.executeUpdate();
@@ -78,21 +96,19 @@ public class DBConn {
 		}
 	}
 
+	public ResultSet execQuery(final String strSQL, final Object[] params) {
 
-	public ResultSet execQuery(final String strSQL,final Object[] params){
-
-
-		System.out.println("SQL:> "+strSQL);
+		System.out.println("SQL:> " + strSQL);
 		try {
-	
+
 			pstmt = conn.prepareStatement(strSQL);
-			//4����̬Ϊpstmt����ֵ
-			for(int i=0; i< params.length ;i++){
-				pstmt.setObject(i+1, params[i]);
-			}		
+			// 4����̬Ϊpstmt����ֵ
+			for (int i = 0; i < params.length; i++) {
+				pstmt.setObject(i + 1, params[i]);
+			}
 
 			rs = pstmt.executeQuery();
-		
+
 			return rs;
 		} catch (SQLException e) {
 			e.printStackTrace();
